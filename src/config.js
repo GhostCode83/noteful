45f45df3442ae2537ee95b945d0bbf413309d9c3
Randy Douglas <randy.e.douglas@gmail.com>
2020-11-06 16:48:43 -0500

    correcting endpoint url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-   API_ENDPOINT: `postgres://sfjawyknjbuodi:a395c9f2a183b3c4cc779c0340682877605fc2f62a74794fc16e6074bb464fa0@ec2-54-84-98-18.compute-1.amazonaws.com:5432/dfr4vppr7i15ls
-   `,
+   API_ENDPOINT: `https://intense-plateau-19623.herokuapp.com/`,
    API_KEY: process.env.REACT_APP_API_KEY
 }
