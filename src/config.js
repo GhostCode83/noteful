9410c04c448081299fb5bf360597eaef68ba876d
Randy Douglas <randy.e.douglas@gmail.com>
2020-11-06 15:25:40 -0500

    ready to deploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-   API_ENDPOINT: `http://localhost:8000/api/` //change to heroku app url
+   API_ENDPOINT: `postgres://sfjawyknjbuodi:a395c9f2a183b3c4cc779c0340682877605fc2f62a74794fc16e6074bb464fa0@ec2-54-84-98-18.compute-1.amazonaws.com:5432/dfr4vppr7i15ls
+   `
 }
