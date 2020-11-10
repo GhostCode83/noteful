265282c8b2251c2e6fdb5c5cd714d43c305db15c
Randy Douglas <randy.e.douglas@gmail.com>
2020-11-10 11:15:27 -0500

    changed endpoint to heroku for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-   API_ENDPOINT: `http://localhost:8000/api/`,
-   //`https://nameless-spire-25810.herokuapp.com/`,
+   API_ENDPOINT: `https://nameless-spire-25810.herokuapp.com/api/`,
    API_KEY: process.env.REACT_APP_API_KEY
 }
